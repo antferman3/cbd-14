5056200b6f093a511d46d46932a6b5aeedd9c039
antferman <aferman993@gmail.com>
2021-04-30 13:47:22 +0200

    configuracion

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,15 +1,16 @@
-package controllers;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import entities.Book;
-import repository.BookRepository;
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
 
 @Controller
 public class BookController {
@@ -22,12 +23,14 @@ public class BookController {
 		return "index";
 	}
 	
-	@PostMapping (path="/add")
-	public @ResponseBody Book addNewBook(@RequestParam String name) {
-		Book book = new Book(name);
-		bookRepository.save(book);
-		return book;
-	}
+	@PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name) {
+      // @ResponseBody means the returned String is the response, not a view name
+      // @RequestParam means it is a parameter from the GET or POST request
+      Book n = new Book(name);
+      bookRepository.save(n);
+      return "Saved";
+    }
 	
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<Book> getAllBooks(){
--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,8 +1,8 @@
-package repository;
+package com.example.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import entities.Book;
+import com.example.demo.entity.Book;
 
 public interface BookRepository extends CrudRepository<Book,Long>{
 
